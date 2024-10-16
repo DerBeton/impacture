1be7e7ac086b7790aedf20a3acd04bedac622c41
Ramon <ramongrutschnig@gmail.com>
2024-10-16 12:14:49 +0200

    feat: implement first version of vote card

---
+++ src/components/voting/VoteButtons.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="vote-button" v-if="type === 'yes'" @click="$emit('yes')">
+    Ja
+  </button>
+  <button class="vote-button" v-else-if="type === 'no'" @click="$emit('no')">
+    Nein
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  type: 'yes' | 'no'
+}>()
+
+const emit = defineEmits(['yes', 'no'])
+</script>
+
+<style lang="scss" scoped>
+.vote-button {
+  @include button-default;
+
+  width: 9.4rem;
+}
+</style>
---
+++ src/views/VoteView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="vote-view">
+    <h1 class="title">
+      Abstimmungen <span>---></span><br />
+      was stimmst du ab?
+    </h1>
+    <VoteCard
+      question="Sollen Atomkraftwerke weiterentwickelt werden?"
+    ></VoteCard>
+    <button class="restart"><--- Restart</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import VoteCard from '@/components/voting/VoteCard.vue'
+</script>
+
+<style lang="scss" scoped>
+.vote-view {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  padding: 4.5rem 6rem;
+  background-color: var(--color-accent-main);
+
+  > .title {
+    @include text-xl;
+  }
+
+  > .restart {
+    @include button-default;
+
+    align-self: baseline;
+    margin-top: auto;
+  }
+}
+</style>
