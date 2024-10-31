169cb9c712e56f3a76ff397ff900fcab967c98ba
Ramon <ramongrutschnig@gmail.com>
2024-10-31 10:33:25 +0100

    feat: add animated year to vision-ui

--- src/views/VisionView.vue
+++ src/views/VisionView.vue
@@ -1,14 +1,34 @@
 <template>
-  <FutureCanvas :votes="$route.params.id" class="canvas-element"></FutureCanvas>
+  <div class="future-view">
+    <DateStatus
+      :date="new Date(2222, 10, 5)"
+      :is-animated="true"
+      class="status"
+    ></DateStatus>
+    <FutureCanvas :votes="$route.params.id" class="canvas"></FutureCanvas>
+  </div>
 </template>
 
 <script setup lang="ts">
 import FutureCanvas from '@/components/FutureCanvas.vue'
+import DateStatus from '@/components/home/DateStatus.vue'
 </script>
 
 <style lang="scss" scoped>
-.canvas-element {
-  width: 100%;
-  height: 100%;
+.future-view {
+  position: relative;
+  padding: 1.75rem;
+
+  > .status {
+    margin: 1.75rem;
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+
+  > .canvas {
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
