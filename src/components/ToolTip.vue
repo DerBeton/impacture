0b62aa714006f296fd325b0c89ae259d3d5436ad
Ramon <ramongrutschnig@gmail.com>
2024-11-01 14:21:00 +0100

    feat: change tooltip style and update position

--- src/components/ToolTip.vue
+++ src/components/ToolTip.vue
@@ -1,5 +1,6 @@
 <template>
-  <div @click="toggleOpen()" class="tooltip-comp">
+  <div class="tooltip-comp">
+    <div @click="toggleOpen()" class="dot"></div>
     <div v-if="isOpen" class="content">
       <h4 v-if="title" class="title">{{ title }}</h4>
       <p v-if="text" class="text">{{ text }}</p>
@@ -10,10 +11,11 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-defineProps({
-  title: String,
-  text: String,
-})
+const props = defineProps<{
+  title?: string
+  text?: string
+  animate?: boolean
+}>()
 
 const isOpen = ref<boolean>(false)
 
@@ -25,32 +27,45 @@ function toggleOpen() {
 <style lang="scss">
 .tooltip-comp {
   position: relative;
-  z-index: 10;
-  width: 1.5rem;
-  height: 1.5rem;
-  border-radius: 100%;
-  background-color: var(--color-accent-main);
-  box-shadow: 0px 0px 8px 3px rgba(255, 255, 255, 0.6);
-  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  > .dot {
+    position: absolute;
+    z-index: 10;
+    width: 1.3rem;
+    height: 1.3rem;
+    border-radius: 100%;
+    background-color: var(--color-accent-main);
+    box-shadow: 0px 0px 8px 3px rgba(255, 255, 255, 0.6);
+    cursor: pointer;
+    animation: pulse 1s ease-in-out infinite alternate;
+  }
 
   > .content {
     @include border-md;
 
-    z-index: -1;
     position: absolute;
     width: 24rem;
-    margin: 0 auto;
     padding: 1rem;
     border: 1px solid black;
     background-color: white;
-
-    left: 50%;
-    bottom: -0.5rem;
-    transform: translate(-50%, 100%);
+    margin-top: 2.5rem;
+    transform: translateY(50%);
 
     > * {
       margin: 0;
     }
+  }
+}
+
+@keyframes pulse {
+  from {
+    scale: 1;
+  }
+  to {
+    scale: 1.2;
   }
 }
 </style>
