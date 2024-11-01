5ee5ea4d4190e0209c0f9ea36122a68a4b65c4c7
Ramon <ramongrutschnig@gmail.com>
2024-11-01 17:45:36 +0100

    chore: update bio scene with demo tooltips

--- src/experience/world/visions/BioVision.ts
+++ src/experience/world/visions/BioVision.ts
@@ -6,7 +6,7 @@ import type { GLTF } from 'three/examples/jsm/Addons.js'
 import Tooltip from '../Tooltip'
 import type { ResourceItem } from '@/experience/utils/Resources'
 // @ts-ignore
-import scene from '@/assets/models/gltf/scenes/bio_scene_2.glb'
+import scene from '@/assets/models/gltf/scenes/bio_scene_3.glb'
 import VisionLoader from './VisionLoader'
 
 const RESOURCE_NAME = 'vision'
@@ -35,7 +35,22 @@ export default class BioVision extends VisionLoader {
       'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia dolores sed libero nam quaerat, aperiam perspiciatis fuga, eveniet vero vitae itaque, dicta fugit expedita! Soluta praesentium magni enim eius fugit!',
     )
     this.addToolTip(
-      this.getObjectByName('Gardenbed'),
+      this.getObjectByName('Gardenbedstone1'),
+      'Gardenbed',
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia dolores sed libero nam quaerat, aperiam perspiciatis fuga, eveniet vero vitae itaque, dicta fugit expedita! Soluta praesentium magni enim eius fugit!',
+    )
+    this.addToolTip(
+      this.getObjectByName('Gardenbedstone2'),
+      'Gardenbed',
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia dolores sed libero nam quaerat, aperiam perspiciatis fuga, eveniet vero vitae itaque, dicta fugit expedita! Soluta praesentium magni enim eius fugit!',
+    )
+    this.addToolTip(
+      this.getObjectByName('Solarpanel'),
+      'Gardenbed',
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia dolores sed libero nam quaerat, aperiam perspiciatis fuga, eveniet vero vitae itaque, dicta fugit expedita! Soluta praesentium magni enim eius fugit!',
+    )
+    this.addToolTip(
+      this.getObjectByName('biogasgrundriss'),
       'Gardenbed',
       'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia dolores sed libero nam quaerat, aperiam perspiciatis fuga, eveniet vero vitae itaque, dicta fugit expedita! Soluta praesentium magni enim eius fugit!',
     )
