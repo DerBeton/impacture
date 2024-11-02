72a8ddef58f2d5c8329fde21ebcf5e48fa3f55d6
Ramon <ramongrutschnig@gmail.com>
2024-11-02 22:32:42 +0100

    chore: update texts on bio vision tooltips

--- src/experience/world/visions/BioVision.ts
+++ src/experience/world/visions/BioVision.ts
@@ -31,28 +31,38 @@ export default class BioVision extends VisionLoader {
   public onReady(): void {
     this.addToolTip(
       this.getObjectByName('Antenna'),
-      'Antenna',
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia dolores sed libero nam quaerat, aperiam perspiciatis fuga, eveniet vero vitae itaque, dicta fugit expedita! Soluta praesentium magni enim eius fugit!',
+      'Stromnetz und Kommunikation',
+      'Die Energie wird von jedem Wohnquartier selbst produziert. Der Wirtschaftskollaps hat dazu geführt, dass alle aufeinander acht geben, da viele in die Armut getrieben wurden. Deshalb wird auch der Strom mit den Nachbarn geteilt. So erhöht sich ausserdem die Versorgungssicherheit der einzelnen Haushalte.',
     )
     this.addToolTip(
-      this.getObjectByName('Gardenbedstone1'),
-      'Gardenbed',
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia dolores sed libero nam quaerat, aperiam perspiciatis fuga, eveniet vero vitae itaque, dicta fugit expedita! Soluta praesentium magni enim eius fugit!',
+      this.getObjectByName('Patchworkblau'),
+      'Patchwork',
+      'Da die Erzeugung von klimaschädlichen Strom verboten wurde, musste auf erneuerbare Energien umgestiegen werden. Der nachhaltige Umgang mit Ressourcen wurde plötzlich sehr wichtig. Rohstoffe stehen nicht mehr unerschöpflich zur Verfügung. Deshalb haben die Bewohner*innen angefangen, alle Baustoffe aus Wastefields wiederzuverwenden, um ihre Gebäude und Geräte zu reparieren. Das Wissen wird untereinander getauscht und Pläne zu Maschinen stehen frei zugänglich (Open Source) zur Verfügung.',
     )
     this.addToolTip(
-      this.getObjectByName('Gardenbedstone2'),
-      'Gardenbed',
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia dolores sed libero nam quaerat, aperiam perspiciatis fuga, eveniet vero vitae itaque, dicta fugit expedita! Soluta praesentium magni enim eius fugit!',
-    )
-    this.addToolTip(
-      this.getObjectByName('Solarpanel'),
-      'Gardenbed',
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia dolores sed libero nam quaerat, aperiam perspiciatis fuga, eveniet vero vitae itaque, dicta fugit expedita! Soluta praesentium magni enim eius fugit!',
+      this.getObjectByName('Cube039'),
+      'Vertical Farming',
+      'Die Wirtschaftskrise hat in der Gesellschaft zu einem umdenken geführt. Es wurde Vielen bewusst, dass wir abhängig von Grosskonzernen gewesen sind und alleine nicht mehr weit kommen würden. Deshalb begannen die Leute ihre eigene Nahrung anzubauen und mit Communities zu teilen. Für den Anbau wird jeder verfügbare Platz verwendet.',
     )
     this.addToolTip(
       this.getObjectByName('biogasgrundriss'),
-      'Gardenbed',
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia dolores sed libero nam quaerat, aperiam perspiciatis fuga, eveniet vero vitae itaque, dicta fugit expedita! Soluta praesentium magni enim eius fugit!',
+      'Biogas',
+      'Eine Art der Stromproduktion sind Biogasanlagen. Dort werden kompostierbare Dinge wie alte Lebensmittel und Abfälle aus dem Garten in sauberen Strom umgewandelt. Selbst das eigene Abwassersystem ist dort angeschlossen. So kann auch bei schlechtem Wetter Strom produziert werden.',
+    )
+    this.addToolTip(
+      this.getObjectByName('Solarpanel'),
+      'Solaranlagen',
+      'Die am weitesten verbreitete Art um Strom zu gewinnen, ist die Verwendung von Solarmodulen. Durch Jahzehntelanger Forschung in diesem Bereich, liegt der Wirkungsgrad mittlerweile bei knapp 60%. Sie sind deshalb an sehr vielen Orten anzutreffen.',
+    )
+    this.addToolTip(
+      this.getObjectByName('TradingHouse'),
+      'Tauschbox',
+      '"Sharing is Caring" lautet das Motto. Deshalb hat Jeder und Jede eine Art Schrank vor dem Haus. Darin werden nicht mehr benötigte Gegenstände, oder selbst angebaute Lebensmittel zur Verfügung gestellt. Wer etwas benötigt, nimmt sich etwas daraus und legt einen Gegenstand zum Tausch zurück.',
+    )
+    this.addToolTip(
+      this.getObjectByName('Gardenbedstone1'),
+      'Gartenbeet',
+      'In diesem Beet wachsen aktuell Kürbisse. Da die Familie, die hier lebt, nicht alle für sich selbst benötigt, wird ein Teil davon über die Tauschbox weitergegeben. Es findet sich bestimmt Jemand, der ein paar Karotten zum Tauschen hat.',
     )
   }
 }
