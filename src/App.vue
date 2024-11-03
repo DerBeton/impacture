175afb4131006d2b852a2f0c177db933bd59afb0
Ramon <ramongrutschnig@gmail.com>
2024-11-03 21:09:30 +0100

    chore: remove demo code from app

--- src/App.vue
+++ src/App.vue
@@ -6,28 +6,6 @@
 
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-// import { supabase } from './lib/supabaseClient'
-import { onMounted, ref, watch, watchEffect } from 'vue'
-// import type { Database } from './types/database.types'
-
-// const comments = ref<Database['public']['Tables']['comments']['Row'][]>([])
-
-// async function getCountries() {
-//   const { data, error } = await supabase.from('comments').select()
-//   if (error) {
-//     console.warn('Fehler beim laden der Kommentare', error)
-//   } else {
-//     comments.value = data || []
-//   }
-// }
-
-// onMounted(() => {
-//   getCountries()
-// })
-
-// watchEffect(() => {
-//   console.log(comments.value)
-// })
 </script>
 
 <style lang="scss">
