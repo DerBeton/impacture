b2342412dd9f50b0451c00ecf410c189848b7734
Ramon <ramongrutschnig@gmail.com>
2024-11-03 15:55:46 +0100

    test: try to get data from supabase

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,28 @@
 
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HomeView from './views/HomeView.vue'
+// import { supabase } from './lib/supabaseClient'
+import { onMounted, ref, watch, watchEffect } from 'vue'
+// import type { Database } from './types/database.types'
+
+// const comments = ref<Database['public']['Tables']['comments']['Row'][]>([])
+
+// async function getCountries() {
+//   const { data, error } = await supabase.from('comments').select()
+//   if (error) {
+//     console.warn('Fehler beim laden der Kommentare', error)
+//   } else {
+//     comments.value = data || []
+//   }
+// }
+
+// onMounted(() => {
+//   getCountries()
+// })
+
+// watchEffect(() => {
+//   console.log(comments.value)
+// })
 </script>
 
 <style lang="scss">
